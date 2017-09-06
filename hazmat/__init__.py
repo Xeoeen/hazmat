e374ba5be44bd4e73164a6b4dd6a56f2acdb51e3
Xeoeen <Xeoeen@users.noreply.github.com>
2017-09-06 20:59:06 +0200

    Handle some file related errors
    Print error message

--- hazmat/__init__.py
+++ hazmat/__init__.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 from .options import OptionsDispacher
 from . import tmp_utils
+from .output import *
 
 
 def main():
     try:
         OptionsDispacher().run()
+    except FileNotFoundError as e:
+        printError("FileNotFoundError")
+        print(e)
+        print("\tBe aware of relative paths in generator and validator")
+    except PermissionError as e:
+        printError("PermissionError")
+        print(e)
+        print("\t Be aware that some files need executive permission")
     finally:
         tmp_utils.clear_up()
 
